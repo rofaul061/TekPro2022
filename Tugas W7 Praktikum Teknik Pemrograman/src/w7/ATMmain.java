2800d6b274fd8d98ad6df6569cdd241f782ce7e2
rofaul061 <95157673+rofaul061@users.noreply.github.com>
2022-04-07 09:44:18 -0700

    Add files via upload

---
+++ Tugas W7 Praktikum Teknik Pemrograman/src/w7/ATMmain.java
@@ -0,0 +1,13 @@
+package w7;
+
+public class ATMmain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
+
+
+
+ 
