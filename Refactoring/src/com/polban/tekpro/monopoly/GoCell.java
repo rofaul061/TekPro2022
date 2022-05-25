8578287aea9c50ce546b0c1cfe7210a03d2417c0
rofaul061 <rofaul.akrom.tif421@polban.ac.id>
2022-05-25 14:00:14 -0700

    Tugas W13 Praktikum Teknik Pemrograman

---
+++ Refactoring/src/com/polban/tekpro/monopoly/GoCell.java
@@ -0,0 +1,32 @@
+package com.polban.tekpro.monopoly;
+
+public class GoCell extends Cell {
+	private boolean available = true;
+	protected Player owner;
+
+	public GoCell() {
+		super.setName("Go");
+	}
+
+	public void playAction() {
+	}
+	
+	void setName(String name) {
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public Player getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Player player) {
+		this.owner = player;
+	}
+}
